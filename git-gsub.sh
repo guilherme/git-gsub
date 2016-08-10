9729d16e152e47b16612bb87b5f83d09344b3558
Guilherme Reis Campos <guilhermekbsa@gmail.com>
2016-08-10 23:21:37 +1000

    shellscript version

---
+++ git-gsub.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# echo "$1"
+set -f
+
+declare -r pattern=$1
+declare -r value=$2
+
+
+read -a files <<< $(ag -l $pattern)
+
+for file in "${files[@]}"; do
+  declare -a ocurrences;
+   while read -r ocurrence; do
+    ocurrences+=("$ocurrence")
+  done < <(ag $pattern $file)
+
+  for ocurrence in "${ocurrences[@]}"; do
+    echo "$file:"
+    line=$(echo "$ocurrence" | awk -F: '{ print $1 }' )
+    content=$(echo "$ocurrence" | awk -F: '{ print $2 }' )
+    echo "-${line}:${content}";
+    echo "+${line}:$(echo $content | awk '{gsub("'$pattern'", "'$value'")}1')";
+    read -r -p 'confirm? [y/n]' response;
+    if [ "$response" = "y" ]; then
+      sed -i '' "${line}s/${pattern}/${value}/g" $file
+    fi
+  done;
+done;
